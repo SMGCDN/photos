8c66d02c0fc1253ca16338be1c15b8539c30a338
SMGoro <72185434+SMGoro@users.noreply.github.com>
2023-04-28 21:14:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 # Cloudflare Pages 构建脚本，如果觉得构建慢可以直接注释掉）
 
-# 部署thumbsup环境
-# npm install -g thumbsup 
-# 按照设置文件构建网站
-thumbsup --config config.json
+# 部署thumbsup环境 && 按照设置文件构建网站
+# npm install -g thumbsup && thumbsup --config config.json
