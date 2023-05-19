044e54933cadd6dd8ab4ecf4580a3b781bb8a723
SMGoro <72185434+SMGoro@users.noreply.github.com>
2023-05-20 02:56:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # 安装前置环境
 sudo apt-get update
-sudo apt-get install -y ffmpeg gifsicle dcraw exiftool graphicsmagick-imagemagick-compat --fix-missing
+sudo apt-get install -y zip ffmpeg gifsicle dcraw exiftool graphicsmagick-imagemagick-compat --fix-missing
 
 # 部署thumbsup环境 && 按照设置文件构建网站
 npm install -g thumbsup && thumbsup --config config.json
