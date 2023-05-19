6b3478a230eea5d2f3a14cd5d13fee6b200e3b6e
SMGoro <72185434+SMGoro@users.noreply.github.com>
2023-05-20 02:56:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-# Cloudflare Pages 构建脚本，如果觉得构建慢可以直接注释掉）
+# 构建脚本，如果觉得构建慢可以直接注释掉）
 
 # 安装前置环境
-sudo apt-get update
-sudo apt-get install -y zip ffmpeg gifsicle dcraw exiftool graphicsmagick-imagemagick-compat --fix-missing
+apt-get update
+apt-get install -y zip ffmpeg gifsicle dcraw exiftool graphicsmagick-imagemagick-compat --fix-missing
 
 # 部署thumbsup环境 && 按照设置文件构建网站
 npm install -g thumbsup && thumbsup --config config.json
